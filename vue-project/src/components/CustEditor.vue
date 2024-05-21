e9bcfb2653f4aec8632124612041cd31fc2d0393
chen <2913173579@qq.com>
2024-05-22 00:33:14 +0800

    编辑器编写

---
+++ vue-project/src/components/CustEditor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="customcomp-editor">
+    <toolbar :editor="editorRef" />
+    <!--插槽-->
+    <slot />
+    <editor
+      node="simple"
+      v-model="valueHtml"
+      @onCreated="(ref) => (editorRef = ref)"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, shallowRef } from "vue";
+import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
+const valueHtml = ref("<p> hello </p>");
+const editorRef = shallowRef();
+</script>
+<style lang="less">
+@import "@wangeditor/editor/dist/css/style.css";
+.customcomp-editor {
+  width: 100%;
+}
+</style>
